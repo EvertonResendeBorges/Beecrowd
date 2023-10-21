8a18dcc49e7479bed4e700ee6681b9842a1b3831
Everton Resende Borges <evertonrborges12@gmail.com>
2023-10-21 20:31:05 -0300

    Hello World!

---
+++ Java/hello_world.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class hello_world {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      System.out.println ("Hello World!");
+ 
+    }
+ 
+}
